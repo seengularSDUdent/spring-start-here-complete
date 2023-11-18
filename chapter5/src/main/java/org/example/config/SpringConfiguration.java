ba057f426021a5a6ba7ed4d1fead51de39a045d6
seengularSDUdent <200107026@stu.sdu.edu.kz>
2023-11-18 09:22:33 +0600

    Bean's scope now changed to Protorype scope, then tested

--- chapter5/src/main/java/org/example/config/SpringConfiguration.java
+++ chapter5/src/main/java/org/example/config/SpringConfiguration.java
@@ -2,15 +2,17 @@ package org.example.config;
 
 import org.example.service.JustSomeService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
 @Configuration
+@ComponentScan(basePackages = "org.example.service")
 public class SpringConfiguration {
 
-    @Bean
-    @Lazy
-    JustSomeService justSomeService(){
-        return new JustSomeService();
-    }
+//    @Bean
+//    @Lazy
+//    JustSomeService justSomeService(){
+//        return new JustSomeService();
+//    }
 }
--- chapter5/src/main/java/org/example/service/JustSomeService.java
+++ chapter5/src/main/java/org/example/service/JustSomeService.java
@@ -1,5 +1,11 @@
 package org.example.service;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class JustSomeService {
     public JustSomeService(){
         System.out.println("The bean is created!");
