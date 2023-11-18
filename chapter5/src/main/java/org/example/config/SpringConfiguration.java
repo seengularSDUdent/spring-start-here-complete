504357912904731baf39e4157cbdf69cccf24044
seengularSDUdent <200107026@stu.sdu.edu.kz>
2023-11-18 09:09:51 +0600

    lazy instantiation demonstration

--- chapter5/src/main/java/org/example/config/SpringConfiguration.java
+++ chapter5/src/main/java/org/example/config/SpringConfiguration.java
@@ -3,11 +3,13 @@ package org.example.config;
 import org.example.service.JustSomeService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class SpringConfiguration {
 
     @Bean
+    @Lazy
     JustSomeService justSomeService(){
         return new JustSomeService();
     }
