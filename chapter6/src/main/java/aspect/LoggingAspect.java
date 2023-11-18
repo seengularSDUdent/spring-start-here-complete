502d989df79e2058b2d35f608c1d1a961a591a32
seengularSDUdent <200107026@stu.sdu.edu.kz>
2023-11-19 00:38:23 +0600

    swapped orderd of execution between two aspects, now first execution is - SecurityAspect then - LogAspect

--- chapter6/src/main/java/aspect/LoggingAspect.java
+++ chapter6/src/main/java/aspect/LoggingAspect.java
@@ -6,12 +6,14 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.logging.Logger;
 
 @Aspect
 @Component
+@Order(2)
 public class LoggingAspect {
 
     private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
--- chapter6/src/main/java/aspect/SecurityAspect.java
+++ chapter6/src/main/java/aspect/SecurityAspect.java
@@ -3,12 +3,14 @@ package aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
 
 @Aspect
 @Component
+@Order(1)
 public class SecurityAspect {
 
     private Logger logger = Logger.getLogger(SecurityAspect.class.getName());
