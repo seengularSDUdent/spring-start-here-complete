c1c679a64480e751fbd0bd612d51d08d4ac1a311
seengularSDUdent <200107026@stu.sdu.edu.kz>
2023-11-22 16:31:14 +0600

    Got a values using path variables

--- chapter8/src/main/java/com/example/chapter8/controller/MainController.java
+++ chapter8/src/main/java/com/example/chapter8/controller/MainController.java
@@ -2,6 +2,7 @@ package com.example.chapter8.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,6 +11,13 @@ public class MainController {
 
     @RequestMapping("/home")
     public String home(@RequestParam(required = false) String color, Model page, @RequestParam String name){
+        page.addAttribute("name", name);
+        page.addAttribute("color", color);
+        return "home.html";
+    }
+
+    @RequestMapping("home/{name}/{color}")
+    public String home2(@PathVariable String name, @PathVariable String color, Model page){
         page.addAttribute("name", name);
         page.addAttribute("color", color);
         return "home.html";
